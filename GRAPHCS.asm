c2d4cada09b3b4f0c007fc32a8549d8a2843020d
omrihhh <0mri.d3v@gmail.com>
2021-01-11 00:40:12 +0200

    added maze printing functionalty

--- GRAPHCS.asm
+++ GRAPHCS.asm
@@ -1,7 +1,7 @@
 ;------------------------------------------
 ; PURPOSE : Test number 1 
 ; SYSTEM  : Turbo Assembler Ideal Mode  
-; AUTHOR  : Omri Hulaty and Aylon Moyal
+; AUTHOR  : Omri Hulaty and Aylon Moyal (and Yuval Rosen)
 ;------------------------------------------
 
 		IDEAL
@@ -12,12 +12,27 @@
 
 		DATASEG
 
-                cell_size equ 16
-                color equ 07h
+                tile equ 16
+                spacing equ 4
+                step equ 16
+                color db 0fh
                 pos_x dw 0
-                pos_y db 0
-		
+                pos_y dw 0
+                player_print db 1
+                maze db 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "n"
+                     db 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, "n"
+                     db 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, "n"
+                     db 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, "n"
+                     db 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, "n"
+                     db 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, "n"
+                     db 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, "n"
+                     db 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, "n"
+                     db 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, "n"
+                     db 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, "n"
+                     db 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, "n"
+                     db 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "n"
 		CODESEG
+
 proc GetInput
     mov ah, 1
     int 16h
@@ -51,64 +66,160 @@ proc GetInput
         jmp Exit
         ret
     up_pressed:
-	sub [byte pos_y], 5
+    cmp [pos_y], 10
+    jl next_k
+	sub [word pos_y], step
         ret
     down_pressed:
-	add [byte pos_y], 5
-        ret
+    cmp [pos_y], 192 - step - tile
+    jg next_k
+	add [word pos_y], step
+    ret
     left_pressed:
-	sub [word pos_x], 5
+    cmp [pos_x], 2
+    jl next_k
+	sub [word pos_x], step
         ret
-    right_pressed: 
-	add [word pos_x], 5
+    right_pressed:
+    cmp [pos_x], 320 - step - tile
+    jg next_k
+	add [word pos_x], step
         ret
     next_k:
         ret
 endp
 
-proc DrawPix
-        push bp
-        mov bp, sp
+proc DrawPix ; get x cord in cx and y cord in dx
         push ax
-        push cx
-        push dx
-        mov ah,0ch;
-        mov al, color
-        mov cx, [bp + 6] ; x co-ordinate
-        mov dx, [bp + 4] ; y co-ordinate
-        push bx
-        mov bh,1    ; page no - critical while animating
-        pop bx
+        mov ah, 0ch
+        mov al, [color]
         int 10h 
-        pop dx
-        pop cx
         pop ax
-        pop bp
-        ret 4
+        ret
 endp DrawPix
 
-proc DrawCell
-        ; shl dx, 1
-        ; shl bx, 1
-        ;------
-        add dx, cell_size 
-        add bx, cell_size  
-        mov cx, cell_size
-        Y:
-        push cx
-        mov cx, cell_size 
-        X:
+
+
+proc DrawTile
         push dx
-        push bx
-        call DrawPix
-        dec dx
-        loop X
-        dec bx
-        add dx, cell_size 
+        push ax
+        push cx
+        cmp [player_print], 1
+        je TilePrint
+        shl dx, spacing
+        shl ax, spacing
+        jmp TilePrint
+        ;------
+    TilePrint:
+        add dx, tile ; y
+        add ax, tile ; x
+        mov cx, tile 
+        Y:
+            push cx
+            mov cx, tile
+            X:
+                push cx
+                mov cx, ax
+                dec cx
+                dec dx
+                call DrawPix
+                inc dx
+                pop cx
+                dec ax
+                loop X
+            dec dx
+            add ax, tile
+            pop cx
+            loop Y
         pop cx
-        loop Y
+        pop ax
+        pop dx
+        ret
+endp DrawTile
+
+proc DrawMaze
+        push cx
+        push dx
+        push ax 
+        xor cx, cx
+        xor ax, ax
+        xor dx, dx
+        mov cx, 12
+        MazeY:
+                push cx
+                mov dx, cx
+                dec dx
+                mov cx, 20
+                MazeX:
+                    push cx
+                    dec cx
+                    mov al, 21
+                    push dx
+                    mul dx
+                    pop dx
+                    add ax, cx
+                    mov bx, ax
+                    add bx, offset maze
+                    mov ax, cx
+                    cmp [byte bx], 1
+                    jne DontFillSpace
+                    mov [player_print], 0
+                    call DrawTile
+                    mov [player_print], 1
+                    jmp DontFillSpace
+                    DontFillSpace:
+                    pop cx
+                    loop mazeX
+                pop cx
+                loop MazeY
+        pop cx
+        pop ax
+        pop dx
         ret 
-endp DrawCell
+endp DrawMaze    
+
+; proc DrawMaze
+;         push bx
+;         push cx
+;         push dx
+
+;         xor dx, dx
+;         xor cx, cx
+
+;         mov [player_print], 0
+
+;         mov bx, offset maze
+;     DrawMazeLoop:
+;         cmp [byte bx], "$"
+;         je FinishMaze
+;         cmp [byte bx], "n"
+;         je NewLine
+;         cmp [byte bx], 0
+;         je Not1
+        
+;         push ax
+;         mov ax, cx
+;         call DrawTile
+;         pop ax
+;         inc cx
+;         inc bx
+;         jmp DrawMazeLoop
+;     Not1:
+;         inc bx
+;         inc cx
+;         jmp DrawMazeLoop
+;     NewLine:
+;         inc dx
+;         xor cx, cx
+;         inc bx
+;         jmp DrawMazeLoop
+;     FinishMaze:
+;         mov [player_print], 1
+;         pop dx
+;         pop cx
+;         pop bx
+;         ret  
+; endp DrawMaze
 
 Start:
         mov ax, @data
@@ -135,23 +246,29 @@ Start:
 
         pop bx
         pop ax
-        
+ 
 MainLoop:
 
-        mov ax, color
-        mov dx, [pos_x]
-        mov bx, [word pos_y]
-        call DrawCell
+        call GetInput
+
+        mov [color], 05h
+        call DrawMaze
+
+        mov [color], 0fh
+        mov ax, [word pos_x]
+        mov dx, [word pos_y]
+        call DrawTile
 
         mov cx, 1
-        mov dx, 0
+        mov dx, 1000
 	    mov ah, 86h
 	    int 15h
 
-        mov ax, 013h
-	    int 10h
-
-        call GetInput
+        mov ax,0600h    ;06 TO SCROLL & 00 FOR FULLJ SCREEN
+        mov bh,0h      ;ATTRIBUTE 7 FOR BACKGROUND AND 1 FOR FOREGROUND
+        mov cx,0h    ;STARTING COORDINATES
+        mov dx,184fh    ;ENDING COORDINATES
+        int 10h
 
         jmp MainLoop
 
